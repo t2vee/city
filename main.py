b87d5a7930cd01605e6f6db8d7f03dc4e5033c38
t2v <me+gitea@t2v.ch>
2023-05-23 20:53:57 +1000

    Added Links and Page switching

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ results = sp.current_user_playing_track()
 
 @app.get("/", response_class=HTMLResponse)
 @limiter.limit("40/minute")
-async def root(request: Request):
+async def root(request: Request, action: str = 'Latest'):
     spot_response = await spot.get_spotify_track()
     if spot_response is None:
         data = ['', '', '', False]
@@ -41,15 +41,15 @@ async def root(request: Request):
                 spot_data["item"]["artists"][0]["external_urls"]["spotify"]]
     files = os.listdir("data/posts")
     files.sort(key=lambda x: os.path.getmtime(os.path.join("data/posts", x)), reverse=True)
-    return templates.TemplateResponse(
-        "new_index.html",
-        {
-            "request": request,
-            "post": files[0].replace(".md", ""),
-            "data": data
-        },
-    )
-
+    match action:
+        case 'Latest':
+            return templates.TemplateResponse("new_index.html", {"request": request, "post": files[0].replace(".md", ""), "data": data}, )
+        case 'v1':
+            return templates.TemplateResponse("old_index.html", {"request": request, "post": files[0].replace(".md", ""), "data": data}, )
+        case 'v2':
+            return templates.TemplateResponse("old_old_index.html", {"request": request, "post": files[0].replace(".md", ""), "data": data}, )
+        case 'v3':
+            return templates.TemplateResponse("index.html", {"request": request, "post": files[0].replace(".md", ""), "data": data}, )
 
 @app.get("/posts")
 async def posts(request: Request):
