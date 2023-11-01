21519340e309146f9cb8a2ea97966a539d5a17b3
t2vee <68762600+t2vee@users.noreply.github.com>
2023-11-02 05:50:32 +1100

    🎶Finally Added Song Statistics Tab!🎵

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ from datetime import datetime, timedelta
 
 cache = {}
 cache_timeout = timedelta(seconds=60)
+stats_cache_timeout = timedelta(days=30)
+stats_cache = {}
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -75,11 +77,60 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "data": spotify_payload, "cached": "Page Being Cached Server Side..." if caching else "Page Cached"}, )
 
 
+def clean_spotify_stat_payload_track(data):
+    simplified_data = {}
+
+    # Extracting relevant data from 'track'
+    track = data.get("track", {})
+    simplified_data["track_id"] = track.get("_id")
+    simplified_data["track_name"] = track.get("name")
+    simplified_data["track_album_id"] = track.get("album")
+    simplified_data["track_artists"] = track.get("artists")
+    simplified_data["track_popularity"] = track.get("popularity")
+
+    # Extracting relevant data from 'artist'
+    artist = data.get("artist", {})
+    simplified_data["artist_id"] = artist.get("_id")
+    simplified_data["artist_name"] = artist.get("name")
+    simplified_data["artist_genres"] = artist.get("genres")
+
+    # Extracting relevant data from 'album'
+    album = data.get("album", {})
+    simplified_data["album_id"] = album.get("_id")
+    simplified_data["album_name"] = album.get("name")
+    simplified_data["album_popularity"] = album.get("popularity")
+    best_period = data.get("bestPeriod", {})
+    simplified_data["best_period"] = best_period
+
+    # Extracting relevant data from 'firstLast'
+    first_last = data.get("firstLast", {})
+    simplified_data["first_played"] = first_last.get("first", {}).get("played_at")
+    simplified_data["last_played"] = first_last.get("last", {}).get("played_at")
+
+    # Extracting relevant data from 'total'
+    total = data.get("total", {})
+    simplified_data["total_count"] = total.get("count")
+
+    return json.dumps(simplified_data)
+
+
+@app.get("/gateway/SpotifyStatsRelay/Track")
+@limiter.limit("1/second")
+def spotify_stats_relay(request: Request, track_id: str):
+    print("reaching spotify stats api")
+    request_url = f"https://spotify.api.apps.t2v.city/track/{track_id}/stats?token={os.getenv('SPOTIFY_API_GUEST_TOKEN')}"
+    response = requests.get(request_url)
+    payload = response.json() if response.status_code == 200 else None
+    cleaned_payload = clean_spotify_stat_payload_track(payload)
+    return cleaned_payload
+
+
 @app.get("/thumbnail/{base64_str}")
 @limiter.limit("3/second")
 def proxy(request: Request, base64_str: str):
     response = requests.get(base64.urlsafe_b64decode(base64_str.encode()).decode(), stream=True)
     return StreamingResponse(response.iter_content(chunk_size=1024), media_type=response.headers["content-type"])
+
 
 ## EXPERIMENTAL
 def minify_css(content: str) -> str:
